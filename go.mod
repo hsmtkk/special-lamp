21b9da2af081c40b903733758dfa583940de3c4b
kouki <kouki@localhost>
2021-05-11 10:11:05 +0900

    session is working

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/hsmtkk/special-lamp
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.3.0
+require (
+	github.com/gorilla/sessions v1.2.1
+	github.com/labstack/echo-contrib v0.9.0
+	github.com/labstack/echo/v4 v4.3.0
+)
--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"text/template"
 
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -23,6 +24,7 @@ func main() {
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
 
 	t := &Template{
 		templates: template.Must(template.ParseGlob("template/*.html")),
@@ -42,19 +44,46 @@ type helloTemplate struct {
 }
 
 func helloGet(c echo.Context) error {
+	msg := "login name & password"
+
+	ses, _ := session.Get("hello-session", c)
+
+	logined, _ := ses.Values["login"].(bool)
+	lname, _ := ses.Values["name"].(string)
+	if logined {
+		msg = "logined: " + lname
+	}
+
 	ht := helloTemplate{
-		Title:   "Send values",
-		Message: "type name and password:",
+		Title:   "Session",
+		Message: msg,
 	}
 	return c.Render(http.StatusOK, "hello", ht)
 }
 
 func helloPost(c echo.Context) error {
+	msg := "login name & password"
+
+	ses, _ := session.Get("hello-session", c)
+	ses.Values["login"] = false
+	ses.Values["name"] = ""
 	nm := c.FormValue("name")
-	pw := c.FormValue("password")
+	pw := c.FormValue("pass")
+	if nm == pw {
+		ses.Values["login"] = true
+		ses.Values["name"] = nm
+	}
+	ses.Save(c.Request(), c.Response())
+
+	logined := ses.Values["login"].(bool)
+	lname := ses.Values["name"].(string)
+	if logined {
+		msg = "logined: " + lname
+	}
+
 	ht := helloTemplate{
-		Title:   "Send values",
-		Message: fmt.Sprintf("name: %s, password: %s", nm, pw),
+		Title:   "Session",
+		Message: msg,
 	}
 	return c.Render(http.StatusOK, "hello", ht)
 }
